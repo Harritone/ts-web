e7042c9e5b05355f85892dcfa2e1588c8f7c9403
Harritone <yourrasik@gmail.com>
2020-12-17 14:35:04 +1000

    Implemented triggering event callbacks

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,14 @@ import { User } from './Models/User';
 
 const user = new User({ name: 'myname', age: 20 });
 
-user.on('change', () => {});
-user.on('change', () => {});
-user.on('aljasf', () => {});
+user.on('change', () => {
+  console.log('Change #1');
+});
+user.on('change', () => {
+  console.log('Change #2');
+});
+user.on('save', () => {
+  console.log('Saving');
+});
 
-console.log(user);
+user.trigger('asdf');
