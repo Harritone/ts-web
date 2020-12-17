fefc00cc7491aa5aae2e928b49208a39a098a82b
Harritone <yourrasik@gmail.com>
2020-12-17 13:55:12 +1000

    Implemented base functionality for User class

--- dist/src.f10117fe.js
+++ dist/src.f10117fe.js
@@ -117,9 +117,52 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   }
 
   return newRequire;
-})({"src/index.ts":[function(require,module,exports) {
-console.log('Hello');
-},{}],"../../../.nvm/versions/node/v14.15.1/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+})({"src/Models/User.ts":[function(require,module,exports) {
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.User = void 0;
+
+var User =
+/** @class */
+function () {
+  function User(data) {
+    this.data = data;
+  }
+
+  User.prototype.get = function (propName) {
+    return this.data[propName];
+  };
+
+  User.prototype.set = function (update) {
+    Object.assign(this.data, update);
+  };
+
+  return User;
+}();
+
+exports.User = User;
+},{}],"src/index.ts":[function(require,module,exports) {
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var User_1 = require("./Models/User");
+
+var user = new User_1.User({
+  name: 'myname',
+  age: 20
+});
+user.set({
+  name: 'newname'
+});
+console.log(user.get('name'));
+console.log(user.get('age'));
+},{"./Models/User":"src/Models/User.ts"}],"../../../.nvm/versions/node/v14.15.1/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log('Hello');
+import { User } from './Models/User';
+
+const user = new User({ name: 'myname', age: 20 });
+
+user.set({ name: 'newname' });
+
+console.log(user.get('name'));
+console.log(user.get('age'));
