5c850eb37adb49334df27d500733a69ec0d816a2
Harritone <yourrasik@gmail.com>
2020-12-18 10:57:18 +1000

    Extracted Eventing logic into separate class and fetching and saving data to another separate class

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,6 @@ import { User } from './models/User';
 
 const user = new User({ name: 'New record', age: 0 });
 
-user.save();
+user.events.on('change', () => console.log('change'));
+
+user.events.trigger('change');
