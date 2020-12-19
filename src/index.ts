daad555ee103794c25b0d268b73889e2b487b8a3
Harritone <yourrasik@gmail.com>
2020-12-19 15:32:53 +1000

    Implemented generic CollectionView class

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,19 @@
-import { UserEdit } from './views/UserEdit';
-import { User } from './models/User';
+import { UserList } from './views/UserList';
+import { Collection } from './models/Collection';
+import { User, UserProps } from './models/User';
 
-const user = User.buildUser({ name: 'NAME', age: 20 });
+const users = new Collection<User, UserProps>(
+  'http://localhost:3000/users',
+  (json: UserProps) => {
+    return User.buildUser(json);
+  }
+);
 
-const root = document.getElementById('root');
+users.on('change', () => {
+  const root = document.getElementById('root');
 
-if (root) {
-  const userEdit = new UserEdit(root, user);
-  userEdit.render();
-} else {
-  throw new Error('Root element not found');
-}
+  if (root) {
+    new UserList(root, users).render();
+  }
+});
+users.fetch();
